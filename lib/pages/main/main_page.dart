37c9cfc11aac5421105bedb4c5d75cd68cd43711
jQuera <rafael2425@gmail.com>
2023-07-16 09:49:39 -0400

    routes

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
-
+  static const String route = "/";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,16 +1,18 @@
+import 'package:flutter_game_chilequiz/pages/error/error_page.dart';
 import 'package:flutter_game_chilequiz/pages/main/main_page.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static GoRouter router() {
     return GoRouter(
-      initialLocation: '/',
+      initialLocation: MainPage.route,
       routes: [
         GoRoute(
           path: '/',
           builder: (context, state) => const MainPage(),
         ),
       ],
+      errorBuilder: (context, state) => const ErrorPage(),
     );
   }
 }
